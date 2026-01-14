f851482d1ea6b244d0805cd655061d2b35aa3a7e
avrielvrl <avrielkhairunnisa6@gmail.com>
2026-01-15 06:32:11 +0700

    tes

--- database.php
+++ database.php
@@ -1,11 +1,14 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 $host = "mainline.proxy.rlwy.net";
 $user = "root";
 $pass = "BNWNmKoNCrGGmWbucAXQZKuRVeWjWkIm";
 $db   = "railway";
 $port = "18268";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     http_response_code(500);
@@ -15,3 +18,4 @@ if (!$conn) {
     ]);
     exit;
 }
+?>
